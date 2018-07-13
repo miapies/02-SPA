6ba619e73d912fdb724446d14fad77e27ba6e9c3
Jimenez Jimenez <m.jimenez.jimenez@accenture.com>
2018-07-13 12:59:22 +0200

    SPA finalizado

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styleUrls: ['./heroe-tarjeta.component.css']
+})
+export class HeroeTarjetaComponent {
+  @Input() heroe: any = {};
+  @Input() index: number;
+
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+  constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
+
+  verHeroe() {
+    this.router.navigate(['/heroe', this.index]);
+    // this.heroeSeleccionado.emit(this.index);
+  }
+}
